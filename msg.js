1e0f0f1915d96fdf25b650239960f8b32f831191
linuxKernal <89059811+linuxKernal@users.noreply.github.com>
2021-10-24 21:30:44 +0530

    Add files via upload

---
+++ msg.js
@@ -0,0 +1,95 @@
+const messages = [
+    {
+        team:'tamil_vijay',
+        chat:[
+            {
+                name:'tamil',
+                msg:"hi vijay how are you ?",
+                time:'12.24 am'
+            },
+            {
+                name:'vijay',
+                msg:"yes tamil i am fine",
+                time:"12.26 am"
+            },
+            {
+                name:'vijay',
+                msg:"i am learing javascript",
+                time:"12.30 am"
+            },
+            {
+                name:'tamil',
+                msg:"i already know javascript",
+                time:"12.31 am"
+            }
+        ]
+    },
+            {
+                team:'ram_vijay',
+                chat:[
+                    {
+                        name:'ram',
+                        msg:"hi vijay how are you ?",
+                        time:'12.24 am'
+                    },
+                    {
+                        name:'ram',
+                        msg:"Hello World",
+                        time:"12.26 am"
+                    },
+                    {
+                        name:'vijay',
+                        msg:"i am learing javascript",
+                        time:"12.30 am"
+                    },
+                    {
+                        name:'ram',
+                        msg:"i already know javascript",
+                        time:"12.31 am"
+                    }
+                 ]
+                } ,
+                    {
+                        team:'tamil_ram',
+                        chat:[
+                            {
+                                name:'tamil',
+                                msg:"hi vijay how are you ?",
+                                time:'12.24 am'
+                            },
+                            {
+                                name:'ram',
+                                msg:"yes ram i am fine",
+                                time:"12.26 am"
+                            },
+                            {
+                                name:'tamil',
+                                msg:"i am learing javascript",
+                                time:"12.30 am"
+                            },
+                            {
+                                name:'ram',
+                                msg:"i already know javascript",
+                                time:"12.31 am"
+                            }
+                            ,
+                            {
+                                name:'tamil',
+                                msg:"i already know javascript",
+                                time:"12.31 am"
+                            },
+                            {
+                                name:'ram',
+                                msg:"welcome to javascript programming language and also i love python",
+                                time:"12.31 am"
+                            },
+                            {
+                                name:'ram',
+                                msg:"i already know javascript",
+                                time:"12.31 am"
+                            }
+                        ]
+                    }
+]
+
+export default messages
